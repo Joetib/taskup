cfbbba17c4d9f1f76b294afb7c4a3f776de33858
MaxzyRich @team_cv <maxzyrich@MaxzyRichs-MacBook-Pro.local>
2021-08-03 15:51:51 +0000

    Fix adding contributors to project

---
+++ src/components/ProjectContributors.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container-fluid shadow py-3">
+    <h4>Contributors</h4>
+    <hr />
+    <div>
+      <select v-model="selected_user" list="users-list" class="form-select">
+        <option  v-for="user in all_users" :key="user.id" :value="user.id">
+          {{ user.name }} ({{ user.email }})
+        </option>
+      </select>
+
+      <button @click="add_selected_user" class="btn mt-2 w-100 btn-primary">Add</button>
+    </div>
+    <p class="text-danger">{{ error_message }}</p>
+    <div class="" v-for="user in users" :key="user.id">
+      <!-- <img
+        src=""
+        class="img-thumbnail rounded-circle"
+        style="width: 60px; height: 60px"
+      /> -->
+      <div class="border border-top-0 border-start-0 border-end-0 p-2">
+        <h6 class="m-0">{{ user.name }}</h6>
+        <small>{{ user.email }}</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: [],
+      selected_user: null,
+      all_users: [],
+      error_message: "",
+    };
+  },
+  props: {
+    project_id: null,
+  },
+  methods: {
+    add_selected_user(){
+
+        if (this.selected_user){
+            axios.post(`project/${this.project_id}/add-contributor/`, {
+                contributors: [this.selected_user]
+            }).then(e => {
+              if (e.data.success){
+                this.fetch_project_contributors()
+              } else {
+                this.error_message = "Could not add Contributor";
+              }
+            }).catch(e => {
+              this.error_message = "Could not add Contributor";
+              console.log(e);
+            })
+        }
+    },
+    fetch_project_contributors() {
+      axios
+        .get(`project/${this.project_id}/contributors/`)
+        .then((e) => {
+          console.log(e.data);
+          if (e.data.success) {
+            this.users = e.data.results;
+            
+            this.all_users = e.data.all_users;
+          } else {
+            this.error_message = e.data.message;
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+          this.error_message = "Sorry could not load contributors";
+          alert("An error occurred when fetching users");
+        });
+    },
+  },
+  mounted() {
+    this.fetch_project_contributors();
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/ProjectDetail.vue
+++ src/views/ProjectDetail.vue
@@ -6,28 +6,37 @@
         <div class="container">
           <ul class="list-unstyled">
             <li class="py-2">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <i class="fa fa-file"></i> Dashboard
-              </a>
+              <router-link
+                class="nav-link d-flex align-items-center gap-2"
+                to="/"
+              >
+                <i class="fa fa-home"></i> Home
+              </router-link>
             </li>
             <li class="py-2">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <i class="fa fa-comment-alt"></i> Chats
-              </a>
+              <router-link
+                class="nav-link d-flex align-items-center gap-2"
+                to="/dashboard"
+              >
+                <i class="fa fa-file"></i> Dashboard
+              </router-link>
             </li>
           </ul>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus alias
-          aut iusto consequuntur ex eaque, neque quia delectus explicabo
-          distinctio, nesciunt inventore a culpa labore sint temporibus ipsam
-          omnis at?
         </div>
       </div>
       <!-- Sidebar end -->
-      <div class="col-lg-8">
+      <div class="col-lg-7">
         <router-view />
-        <div class="container py-5">
-          <h2>{{ project.name }}</h2>
-          <p>{{ project.description }}</p>
+        <div class="container py-5 d-flex justify-content-between">
+          <div>
+            <h2>{{ project.name }}</h2>
+            <p>{{ project.description }}</p>
+          </div>
+          <div>
+            <DeleteProjectButton
+              v-bind:project_id="project_id"
+            ></DeleteProjectButton>
+          </div>
         </div>
         <div class="container">
           <div class="d-flex justify-content-start align-items-center">
@@ -36,9 +45,9 @@
             </a>
           </div>
           <div class="row py-4">
-              <div class="col-12">
-                  <h2>Tasks</h2>
-              </div>
+            <div class="col-12">
+              <h2>Tasks</h2>
+            </div>
             <TaskCard
               v-for="task in project.tasks"
               :key="task.id"
@@ -47,14 +56,17 @@
           </div>
         </div>
       </div>
-      <div class="col-lg-2 p-1">
+      <div class="col-lg-3 p-0">
         <div class="container-fluid py-5">
-          
+          <ProjectContributors v-bind:project_id="project_id"></ProjectContributors>
         </div>
       </div>
     </div>
     <div class="full-screen-form-overlay" v-if="open_create_task_dialog">
-      <CreateTask v-bind:project_id="project_id" @create_task_done="create_task_done" />
+      <CreateTask
+        v-bind:project_id="project_id"
+        @create_task_done="create_task_done"
+      />
     </div>
   </div>
 </template>
@@ -63,11 +75,14 @@
 import axios from "axios";
 import CreateTask from "../components/CreateTask.vue";
 import TaskCard from "../components/TaskCard.vue";
-
+import DeleteProjectButton from "../components/DeleteProjectButton.vue";
+import ProjectContributors from '../components/ProjectContributors.vue';
 export default {
   components: {
     CreateTask,
     TaskCard,
+    DeleteProjectButton,
+    ProjectContributors,
   },
   data() {
     return {
@@ -88,7 +103,7 @@ export default {
     },
     create_task_done() {
       this.open_create_task_dialog = false;
-      this.fetchProject(this.project.id)
+      this.fetchProject(this.project.id);
     },
     enable_create_task() {
       this.open_create_task_dialog = true;
