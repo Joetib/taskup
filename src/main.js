db3ad5ca99f64f1da70151febfe8717fa9dc7c89
Joetib <otiboatengjoe@gmail.com>
2021-09-10 17:22:16 +0000

    Fixed base url issue for frontend in production

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:5000/'
+if (process.env.NODE_ENV === "development"){
+    axios.defaults.baseURL = 'http://localhost:5000/'
+} else {
+    axios.defaults.baseURL = 'https://joetib.pythonanywhere.com/'
+}
+console.log(axios.defaults.baseURL)
 
 createApp(App).use(store).use(router, axios).mount('#app')
